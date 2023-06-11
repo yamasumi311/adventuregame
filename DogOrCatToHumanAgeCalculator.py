9100a149bcae3ff74763b48154671ff7a2370aa2
yamasumi311 <chim.0.don@gmail.com>
2023-06-11 23:11:19 +1000

    converted int to str

--- DogOrCatToHumanAgeCalculator.py
+++ DogOrCatToHumanAgeCalculator.py
@@ -1,21 +1,18 @@
-import math
-
 animal = input('Enter dog or cat: ')
 while not (animal.lower() == 'dog' or animal.lower() == 'cat'):
-    print('That is invalid input. Enter dog or cat: ')
+    animal = input('That is invalid input. Enter dog or cat: ')
 
 animalAge = input('Enter age of animal in number: ')
-while not (type(animalAge) == int):
-    animaAge = input('That is invalid input. Enter age of animal in number: ')
+animalAge = int(animalAge)
 
-if animal.lower() == dog:
-   if animalAge == 1:
-       print('Human age of dog is 12.')
-   elif animalAge == 2:
-       print('Human age of dog is 24.')
-   else:
-       humanAge = 24 + 4 * (animalAge - 2)
-       print('Human age of dog is' + humanAge +'.')
+if animal.lower() == 'dog':
+    if animalAge == 1:
+        print('Human age of dog is 12.')
+    elif animalAge == 2:
+        print('Human age of dog is 24.')
+    else:
+        humanAge = 24 + 4 * (animalAge - 2)
+        print('Human age of dog is ' + str(humanAge) + '.')
 else:
     if animalAge == 1:
         print('Human age of cat is 15.')
@@ -23,6 +20,5 @@ else:
         print('Human age of cat is 24.')
     else:
         humanAge = 24 + 4 * (animalAge - 2)
-        print('Human age of dog is' + humanAge + '.')
-
+        print('Human age of cat is ' + str(humanAge) + '.')
 
