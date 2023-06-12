7dd0c662735fabb67b66458754c948558808c43b
yamasumi311 <chim.0.don@gmail.com>
2023-06-12 14:53:26 +1000

    made float as valid number

--- DogOrCatToHumanAgeCalculator.py
+++ DogOrCatToHumanAgeCalculator.py
@@ -6,25 +6,26 @@ while not (animal.lower() == 'dog' or animal.lower() == 'cat'):
 # check if input is number
 while True:
     try:
-        animalAge = int(input('Enter age of animal in number: '))
+        animalAge = float(input('Enter age of animal in number: '))
         break
     except:
         print('That is invalid input. Try again.')
         pass
 
 if animal.lower() == 'dog':
-    if animalAge == 1:
-        print('Human age of dog is 12.')
-    elif animalAge == 2:
-        print('Human age of dog is 24.')
+    if animalAge <= 2:
+        humanAge = 12 * animalAge
+        print('Human age of dog is' + str(humanAge) + '.')
     else:
         humanAge = 24 + 4 * (animalAge - 2)
         print('Human age of dog is ' + str(humanAge) + '.')
 else:
-    if animalAge == 1:
-        print('Human age of cat is 15.')
-    elif animalAge == 2:
-        print('Human age of cat is 24.')
+    if animalAge <= 1:
+        humanAge = 15 * animalAge
+        print('Human age of cat is ' + str(humanAge) + '.')
+    elif 1 < animalAge <= 2:
+        humanAge = 15 + 9 * (animalAge - 1)
+        print('Human age of cat is ' + str(humanAge) + '.')
     else:
         humanAge = 24 + 4 * (animalAge - 2)
         print('Human age of cat is ' + str(humanAge) + '.')
