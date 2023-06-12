d42dab85263b7f3ae43b1188139ce1e5f0e49178
yamasumi311 <chim.0.don@gmail.com>
2023-06-12 18:33:50 +1000

    pursing csv file using 'csv' module

--- quizGame.py
+++ quizGame.py
@@ -1,38 +1,17 @@
-# pursing csv file without importing 'csv'
-import random
+# pursing CSV file using the 'csv' module
+
+import csv
 score = 0
 
 # open the CSV file
 with open('questionList', 'r') as file:
-    # read all the lines from the file
-    lines = file.readlines()
+    # Create a CSV reader
+    reader = csv.reader(file)
 
-    # Shuffle the lines randomly
-    random.shuffle(lines)
-
-    # Select a specific number of random lines
-    num_lines = 3
-    random_lines = lines[:num_lines]
-
-    # Iterate over each line
-    for line in random_lines:
-        # Remove the newline character(\n) at the end of the line
-        line = line.strip()
-
-        # Split the line by the comma separator
-        values = line.split(', ')
-
+    # Iterate over each row in the CSV file
+    for row in reader:
         # Access the values in each column
-        question = values[0]
-        answer = values[1]
+        question = row[0]
+        answer = row[1]
 
         # Process the data as needed
-
-        user_input = input(f'Question: {question} ').lower()
-        if user_input == answer.lower():
-            print('You are correct!')
-            score = score + 1
-        else:
-            print(f'You are wrong. The answer is {answer}.')
-
-print(f'Your score is {score}')
